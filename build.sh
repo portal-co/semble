e742ace87047b5c092a56fe778465cbe634449a1
Graham Kelly <gkgoat6700@gmail.com>
2025-07-02 16:28:41 +0000

    views

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 cd $(dirname $0)
-npx parcel build ./weak-map
+npx parcel build ./weak-map ./dataview
---
+++ dataview/index.d.ts
@@ -0,0 +1,4 @@
+export const _ArrayBuffer: typeof ArrayBuffer;
+export const _DataView: typeof DataView;
+
+//# sourceMappingURL=index.d.ts.map
