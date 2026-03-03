ea9e689496d752df573af424ad552b9a9e210423
gkgoat <65402989+gkgoat1@users.noreply.github.com>
2026-03-02 22:53:24 -0500

    [AI] zshy 1

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-cd $(dirname $0)
-npx zshy -p tsconfig.json
+cd $(dirname "$0")
+sh ./packages/rt/common/build.sh
+sh ./packages/rt/weak-map/factory/build.sh
+sh ./packages/rt/proxy/factory/build.sh
+sh ./packages/rt/weak-map/build.sh
+sh ./packages/rt/dataview/build.sh
+sh ./packages/rt/proxy/build.sh
